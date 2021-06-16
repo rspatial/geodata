a0ff0ec5d35b17d42e8108af5a2e9820bbc96685
rhijmans <r.hijmans@gmail.com>
2021-06-16 12:49:57 -0700

    abc

---
+++ R/crop_sacks.R
@@ -0,0 +1,29 @@
+
+crop_calendar_sacks <- function(crop="", path=".") {
+
+	stopifnot(dir.exists(path))
+	folder <- file.path(path, "sachs")
+	dir.create(folder, FALSE, FALSE)
+	
+	m <- c("Barley.Winter.crop.calendar.nc.gz", "Barley (winter)", "Barley.crop.calendar.nc.gz", "Barley (spring)", "Cassava.crop.calendar.nc.gz", "Cassava", "Cotton.crop.calendar.nc.gz", "Cotton", "Groundnuts.crop.calendar.nc.gz", "Groundnut", "Maize.crop.calendar.nc.gz", "Maize (main season)", "Maize.2.crop.calendar.nc.gz", "Maize (2nd season)", "Millet.crop.calendar.nc.gz", "Millet", "Oats.Winter.crop.calendar.nc.gz", "Oat (winter)", "Oats.crop.calendar.nc.gz", "Oat (spring)", "Potatoes.crop.calendar.nc.gz", "Potato", "Pulses.crop.calendar.nc.gz", "Pulses", "Rapeseed.Winter.crop.calendar.nc.gz", "Rapeseed", "Rice.crop.calendar.nc.gz", "Rice (main season)", "Rice.2.crop.calendar.nc.gz", "Rice (2nd season)", "Rye.Winter.crop.calendar.nc.gz", "Rye", "Sorghum.crop.calendar.nc.gz", "Sorghum (main season)", "Sorghum.2.crop.calendar.nc.gz", "Sorghum (2nd season)", "Soybeans.crop.calendar.nc.gz", "soybean", "Sugarbeets.crop.calendar.nc.gz", "sugarbeet", "Sunflower.crop.calendar.nc.gz", "Sunflower", "Sweet.Potatoes.crop.calendar.nc.gz", "Sweetpotato", "Wheat.Winter.crop.calendar.nc.gz", "Wheat (winter)", "Wheat.crop.calendar.nc.gz", "Wheat (spring)", "Yams.crop.calendar.nc.gz", "Yam")
+	m <- matrix(m, ncol=2, byrow=TRUE)
+
+	if (!(crop %in% m[,2])) {
+		cat("Choose one of:\n")
+		print(m[,2])
+	} else {
+		i <- which(m[,2] == crop)
+		fout <- file.path(folder, m[i,1])
+		fout2 <- gsub(".gz$", "", fout)
+		if (!file.exists(fout2)) {
+			baseurl <- "https://nelson.wisc.edu/sage/data-and-models/crop-calendar-dataset/netcdf/5min/" 
+			url <- paste0(baseurl, m[i,1])
+			utils::download.file(url, fout, mode="wb")
+			R.utils::gunzip(fout)
+		}
+		r <- terra::rast(fout2)
+		return(r)
+	}
+}
+
+
---
+++ R/crop_spam.R
@@ -0,0 +1,73 @@
+
+spamCrops <- function() {
+	x <- matrix(c("wheat", "whea", "rice", "rice", "maize", "maiz", "barley", "barl", "pearl millet", "pmil", "small millet", "smil", "sorghum", "sorg", "other cereals", "ocer", "potato", "pota", "sweet potato", "swpo", "yams", "yams", "cassava", "cass", "other roots", "orts", "bean", "bean", "chickpea", "chic", "cowpea", "cowp", "pigeonpea", "pige", "lentil", "lent", "other pulses", "opul", "soybean", "soyb", "groundnut", "grou", "coconut", "cnut", "oilpalm", "oilp", "sunflower", "sunf", "rapeseed", "rape", "sesameseed", "sesa", "other oil crops", "ooil", "sugarcane", "sugc", "sugarbeet", "sugb", "cotton", "cott", "other fibre crops", "ofib", "arabica coffee", "acof", "robusta coffee", "rcof", "cocoa", "coco", "tea", "teas", "tobacco", "toba", "banana", "bana", "plantain", "plnt", "tropical fruit", "trof", "temperate fruit", "temf", "vegetables", "vege", "rest of crops", "rest"), ncol=2, byrow=TRUE)
+	colnames(x) <- c("crop", "code")
+	x
+}
+
+
+
+crop_spam <- function(crop="", var="area", path=".", africa=FALSE) {
+	folder <- file.path(path, "spam")
+	stopifnot(var %in% c("area", "yield"))
+	stopifnot(dir.exists(path))
+	dir.create(folder, FALSE, FALSE)
+	crp <- tolower(trimws(crop))
+	crops <- spamCrops()
+	if (!(crp %in% crops)) { stop("crop not know to SPAM; see spamCrops()") }
+	i <- which(crp == crops)[1]
+	if (i > nrow(crops)) i = i - nrow(crops)
+	crp <- toupper(crops[i,2])
+	if (africa) {
+		urlbase <- "https://s3.amazonaws.com/mapspam/2017/ssa/v1.1/geotiff/"	
+	} else {
+		urlbase <- "https://s3.amazonaws.com/mapspam/2010/v1.1/geotiff/"
+	}
+	if (var == "area") {
+		url <- paste0(urlbase, "spam2010v1r1_global_harv_area.geotiff.zip")
+	} else {
+		url <- paste0(urlbase, "spam2010v1r1_global_yield.geotiff.zip")
+	}
+
+	pre <- "spam2010v1r1_global_"
+	if (africa) {
+		afpre <- "spam2017v1r1_ssa_"
+		url <- gsub(pre, afpre, url)
+		pre <- afpre
+	}
+	zipf <- file.path(folder, basename(url))
+	if (!file.exists(zipf)) {
+		utils::download.file(url, zipf, mode="wb")
+	}
+	ff <- utils::unzip(zipf, list=TRUE)
+	fs <- grep(crp, ff$Name, value=TRUE)
+	ffs <- file.path(folder, fs)
+	if (all(!file.exists(ffs))) {
+		utils::unzip(zipf, files=fs, junkpaths=TRUE, exdir=folder)
+	}
+	x <- terra::rast(ffs)
+
+	#nicenms <- c("A", "all", "I", "irrigated", "H", "rainfed-highinput", "L", "rainfed-lowinput", "S", "rainfed-subsistence", "R", "rainfed")
+	#nicenms <- matrix(nicenms, ncol=2, byrow=TRUE)
+	#nicenms <- nicenms[order(nicenms[,1]), ]
+	#if (africa) {
+	#	nicenms <- nicenms[nicenms[,1] %in% c("A", "I", "R"), ]
+	#}
+
+	#n <- sort(names(x))
+	#n <- substr(n, nchar(n[1]), nchar(n[1]))
+	#i <- match(n, nicenms[,1])
+	names(x) <- gsub(pre, "", names(x), ignore.case=TRUE)
+	names(x) <- gsub("gr_", "", names(x), ignore.case=TRUE)
+	
+	terra::ext(x) <- c(-180, 180, -90, 90)
+	if (africa) {
+		x <- crop(x, ext(-26, 58, -35, 26))
+	}
+	if (var=="yield") {
+		x <- classify(x, cbind(0, NA))
+	}
+	x
+}
+
+
