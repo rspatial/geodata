840b3205a195456dccc56104ca5ac44ab59767c4
rhijmans <r.hijmans@gmail.com>
2021-06-20 23:21:28 -0700

    g

--- R/soil.R
+++ R/soil.R
@@ -1,14 +1,15 @@
 
 
-soil_africa <- function(var, depth=20, version=0.13, path) {
+af_soil_isda <- function(var, depth=20, error=FALSE, path) {
 
-	stopifnot(depth %in% c(20,50))
+	stopifnot(depth %in% c(20, 50))
 	
-	stopifnot(var %in% c("ph_h2o", "log.ecec.f", "log.oc", "log.ca_mehlich3", "log.k_mehlich3", "log.p_mehlich3"))
+	stopifnot(var %in% c("al", "bdr", "clay", "c.tot", "ca", "db.od", "ecec.f", "fe", "k", "mg", "n.tot", "oc", "p", "ph", "sand", "silt", "s", "texture", "wpg2", "zn"))
 	stopifnot(dir.exists(path))
+	if (error) var <- paste0(var, "-error")
 	
-	
-	stopifnot(version == 0.13)
+	version=0.13
+	#stopifnot(version == 0.13)
 	
 	depth <- ifelse(depth == 20, "0-20cm", "20-50cm")
 
@@ -16,7 +17,7 @@ soil_africa <- function(var, depth=20, version=0.13, path) {
 	filepath <- file.path(path, filename)
 
 	if (!(file.exists(filepath))) {
-		url <- paste0("https://biogeo.ucdavis.edu/data/geodata/soil/", filename)
+		url <- paste0("https://biogeo.ucdavis.edu/data/geodata/soil/isda/", filename)
 		utils::download.file(url, filepath, mode="wb")
 		r <- try(rast(filepath))
 		if (class(r) == "try-error") {
@@ -27,43 +28,6 @@ soil_africa <- function(var, depth=20, version=0.13, path) {
 		r <- rast(filepath)
 	}
 	r
-}
-
-
-
-soil_grids <- function(var, depth, stat="mean", name="") {
-	sg_url="/vsicurl/https://files.isric.org/soilgrids/latest/data/"
-	var <- var[1]
-	stopifnot(var %in% c("bdod", "cfvo", "clay", "nitrogen", "ocd", "phh2o", "sand", "silt", "soc", "wrb"))
-	if (var == "wrb") {
-		#h <- readLines("https://files.isric.org/soilgrids/latest/data/wrb/")
-		#h <- grep("vrt<", h, value=TRUE)
-		#h <- gsub("<li><a href=\"",  "", h)
-		#h <- strsplit(h, ".vrt\"")
-		#h <- sapply(h, function(i) i[1])
-		h <- c('Acrisols', 'Albeluvisols', 'Alisols', 'Andosols', 'Arenosols', 'Calcisols', 'Cambisols', 'Chernozems', 'Cryosols', 'Durisols', 'Ferralsols', 'Fluvisols', 'Gleysols', 'Gypsisols', 'Histosols', 'Kastanozems', 'Leptosols', 'Lixisols', 'Luvisols', 'Nitisols', 'Phaeozems', 'Planosols', 'Plinthosols', 'Podzols', 'Regosols', 'Solonchaks', 'Solonetz', 'Stagnosols', 'Umbrisols', 'Vertisols') 
-		name <- name[1]
-		if (!(name %in% h)) {
-			stop(paste("choose one of:", paste(h, collapse=", "))) 
-		}
-		u <- file.path(sg_url, var, paste0(name, ".vrt"))
-	} else {
-		depth <- as.character(round(depth[1]))
-		if (var == "ocs") {
-			stopifnot(depth == "30")	
-			dd <- "0-30"
-		} else {
-			dpts <- c("5", "15", "30", "60", "100", "200")
-			if (!(depth %in% dpts)) {
-				stop(paste("depth must be one of:", paste(dpts, collapse=", ")))
-			}
-			dd <- c("0-5", "5-15", "15-30", "30-60", "60-100", "100-200")[depth == dpts]
-		}
-		stat <- stat[1]
-		stopifnot(stat %in% c("mean", "uncertainty", "Q0.05", "Q0.5", "Q0.95"))
-		u <- file.path(sg_url, var, paste0(var, "_", dd, "cm_", stat, ".vrt"))
-	}
-	rast(u)
 }
 
 
