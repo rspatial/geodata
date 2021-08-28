98c1e439723191af6bcb1dfd889e42e88b9bc5c5
rhijmans <r.hijmans@gmail.com>
2021-08-28 14:31:57 +0200

    ph

--- R/soil_isda.R
+++ R/soil_isda.R
@@ -5,7 +5,7 @@ soil_af_isda <- function(var, depth=20, error=FALSE, path) {
 	stopifnot(as.numeric(depth) %in% c(20, 50))
 	depth <- ifelse(depth == 20, "0-20cm", "20-50cm")
 	
-	stopifnot(var %in% c("al", "bdr", "clay", "c.tot", "ca", "db.od", "ecec.f", "fe", "k", "mg", "n.tot", "oc", "p", "ph", "sand", "silt", "s", "texture", "wpg2", "zn"))
+	stopifnot(var %in% c("al", "bdr", "clay", "c.tot", "ca", "db.od", "ecec.f", "fe", "k", "mg", "n.tot", "oc", "p", "ph.h2o", "sand", "silt", "s", "texture", "wpg2", "zn"))
 	stopifnot(dir.exists(path))
 	
 	if (error) var <- paste0(var, "-error")
