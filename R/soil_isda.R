6760928de91d312c90fe1979c5799c7323c89cda
rhijmans <r.hijmans@gmail.com>
2022-01-13 16:21:28 -0800

    fixes #8

--- R/soil_isda.R
+++ R/soil_isda.R
@@ -3,10 +3,27 @@
 soil_af_isda <- function(var, depth=20, error=FALSE, path, ...) {
 	stopifnot(dir.exists(path))
 
-	stopifnot(as.numeric(depth) %in% c(20, 50))
-	depth <- ifelse(depth == 20, "0-20cm", "20-50cm")
+
+	var <- tolower(var)
+	vars <- c("al", "bdr", "clay", "c.tot", "ca", "db.od", "ecec.f", "fe", "k", "mg", "n.tot", "oc", "p", "ph.h2o", "sand", "silt", "s", "texture", "wpg2", "zn")
+	if (!(var %in% vars)) {
+		stop(paste("unknown variable. Use one of:\n", vars))
+	}
 	
-	stopifnot(var %in% c("al", "bdr", "clay", "c.tot", "ca", "db.od", "ecec.f", "fe", "k", "mg", "n.tot", "oc", "p", "ph.h2o", "sand", "silt", "s", "texture", "wpg2", "zn"))
+	if (var %in% c("clay", "silt", "sand")) {
+		var <- paste0(var, ".tot.psa")
+	} else if (var == "n.tot") {
+		var <- "n.tot.ncs"
+	} else if (var == "texture") {
+		var <- "texture.class"	
+	}
+
+	if (var == "bdr") {
+		depth = "0-200cm"
+	} else {
+		stopifnot(as.numeric(depth) %in% c(20, 50))
+		depth <- ifelse(depth == 20, "0-20cm", "20-50cm")
+	}
 	
 	if (error) var <- paste0(var, "-error")
 	
@@ -16,11 +33,13 @@ soil_af_isda <- function(var, depth=20, error=FALSE, path, ...) {
 	filename <- paste0("isda_", var, "_", depth, "_v", version,  "_30s.tif")
 	filepath <- file.path(path, filename)
 
+
 	if (!(file.exists(filepath))) {
-		url <- paste0("https://biogeo.ucdavis.edu/data/geodata/soil/isda/", filename)
+		burl <- "https://geodata.ucdavis.edu/geodata/soil/isda/"
+		url <- file.path(burl, filename)
 		.downloadDirect(url, filepath, ...)
 		if (file.exists(filepath) && grepl("texture", filename)) {
-			url <- paste0("https://biogeo.ucdavis.edu/data/geodata/soil/isda/", paste0(filename, ".aux.xml"))
+			url <- file.path(burl, paste0(filename, ".aux.xml"))
 			.downloadDirect(url, paste0(filepath, ".aux.xml"), ...)
 		}
 		r <- try(rast(filepath))
